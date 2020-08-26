91031eca1a1f41aa545decde9bf5db09abd5b228
Diego Stiehl <seccomiro@gmail.com>
2020-08-26 11:05:25 -0300

    Add FragmentTransaction

--- app/src/main/java/com/stiehl/aulafragments/MainActivity.kt
+++ app/src/main/java/com/stiehl/aulafragments/MainActivity.kt
@@ -2,10 +2,31 @@ package com.stiehl.aulafragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.stiehl.aulafragments.fragments.GalleryFragment
+import com.stiehl.aulafragments.fragments.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val galleryFragment = GalleryFragment()
+
+        useFragment(homeFragment, false)
+
+        btHome.setOnClickListener { useFragment(homeFragment) }
+        btGallery.setOnClickListener { useFragment(galleryFragment) }
+    }
+
+    private fun useFragment(fragment: Fragment, addToBackStack: Boolean = true) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragContainer, fragment)
+            if (addToBackStack)
+                addToBackStack(null)
+            commit()
+        }
     }
 }
